822c31ccedcc205aaf4266538d1c500e77e58d08
蜀葵 <540813165@qq.com>
2019-07-25 10:21:51 +0800

    feat: 更新文档。

--- test.lua
+++ test.lua
@@ -4,7 +4,7 @@ local title = {['ID']=0,['Name']=1,['TextName']=2,['IconID']=3,['NumericalRate']
 local data = {
 { ID = 100, Name = "黑猫警长", TextName = "黑猫警长，", IconID = 0, NumericalRate = 0.6, ItemID = 100, BuffID = {10}, SkillID = {{x=4,y=6}, {x=8,y=9}}, AttackParam = 1, Vector3Test = {{x=3,y=4,z=5}}, Vector2Test = {x=1,y=2}, key = 1, AwardList = {10, 3, 4}, Type = "Leader", Pos = {{X=100,Y=89},{X=20,Y=30}}, PosSingle = {X=100,Y=89}},
 { ID = 101, Name = "葫芦\n娃", TextName = "", IconID = 0, NumericalRate = 0.8, ItemID = 100, BuffID = {3, 1}, SkillID = {}, AttackParam = 0, Vector3Test = {}, Vector2Test = {x=0,y=0}, key = 2, AwardList = {1, 3, 5}, Type = "Pig", Pos = {{X=100,Y=89},{X=20,Y=39}}, PosSingle = {X=100,Y=20}},
-{ ID = 102, Name = "舒\"克\"", TextName = "", IconID = 0, NumericalRate = 0.7, ItemID = 100, BuffID = {}, SkillID = {}, AttackParam = 0, Vector3Test = {}, Vector2Test = {x=0,y=0}, key = 3, AwardList = {}, Type = "Hammer", PosSingle = {}},
+{ ID = 102, Name = "舒\"克\"", TextName = "", IconID = 0, NumericalRate = 0.7, ItemID = 100, BuffID = {}, SkillID = {}, AttackParam = 0, Vector3Test = {}, Vector2Test = {x=0,y=0}, key = 3, AwardList = {}, Type = "Hammer", PosSingle = {X=100,Y=89}},
 { ID = 103, Name = "贝\n塔", TextName = "", IconID = 0, NumericalRate = 0, ItemID = 100, BuffID = {}, SkillID = {}, AttackParam = 0, Vector3Test = {}, Vector2Test = {x=0,y=0}, key = "one", AwardList = {}, Type = "Monkey", PosSingle = {}},
 { ID = 104, Name = "邋遢大王", TextName = "", IconID = 0, NumericalRate = 1, ItemID = 100, BuffID = {}, SkillID = {}, AttackParam = 0, Vector3Test = {}, Vector2Test = {x=0,y=0}, key = 5, AwardList = {}, Type = "Pig", PosSingle = {}}
 }
